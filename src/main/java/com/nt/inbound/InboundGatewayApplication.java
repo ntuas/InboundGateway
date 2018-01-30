893b55f20458dd4f4c26ab2c79ac19eec71bc7f0
Uwe Eisele <uwe.eisele@online.de>
2018-01-30 19:47:22 +0100

    log host ip

--- src/main/java/com/nt/inbound/InboundGatewayApplication.java
+++ src/main/java/com/nt/inbound/InboundGatewayApplication.java
@@ -1,5 +1,7 @@
 package com.nt.inbound;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @RestController
 public class InboundGatewayApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(InboundGatewayApplication.class);
 
 	@Value("${WelcomeMessage:No Welcome Message defined}")
 	private String message;
@@ -21,5 +29,10 @@ public class InboundGatewayApplication {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getMessage() {
 		return this.message;
+	}
+
+	@PostConstruct
+	public void logHostIp() throws UnknownHostException {
+		LOGGER.info("Host Ip Address: {}", InetAddress.getLocalHost().getHostAddress());
 	}
 }
